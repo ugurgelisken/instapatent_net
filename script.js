627394d61666e0caf25f3ce504b357f4a0a2b5e7
ugur.gelisken <ugur.gelisken@ardictech.com>
2024-01-17 16:42:38 +0300

    initial

--- script.js
+++ script.js
@@ -1,78 +1,90 @@
-$(window).on("scroll", function () {
-  toggleBackToTopButton();
+$(document).ready(function () {
+  // Cache selectors for performance
+  const $window = $(window);
+  const $menu = $("#menu");
+  const $menuLinks = $("#menu a");
+  const $backToTopButton = $("#backToTop");
+  const $logo = $("#logo");
+  const $sections = $(".section-box");
+  const $backgroundImage = $(".background-image");
 
-  if ($(window).scrollTop() === 0) {
+  // Scroll event listener
+  $window.on("scroll", function () {
+    toggleBackToTopButton();
+    toggleMenuScrolledClass();
+    setActiveLinkOnScroll();
+    parallaxBackgroundImage();
+  });
+
+  // Click event listeners
+  $menuLinks.on("click", function () {
     removeActiveClassFromLinks();
-  }
+    $(this).addClass("active");
+  });
 
-  const menu = $("#menu");
-  if ($(window).scrollTop() > 0) {
-    menu.addClass("scrolled");
-  } else {
-    menu.removeClass("scrolled");
-  }
+  $(".hamburger, .times").on("click", function () {
+    $menu.toggleClass("active");
+  });
 
-  const sections = $(".section-box");
-  const navLinks = $("#menu .menu-item");
-
-  sections.each(function (index, section) {
-    const rect = section.getBoundingClientRect();
-    if (rect.top > 0 && !(rect.bottom > 0)) {
-      navLinks.removeClass("active");
-      $(navLinks[index]).addClass("active");
-      const id = $(section).attr("id");
-      history.pushState(null, null, "#" + id);
+  $(document).on("click", function (event) {
+    if (!$(".hamburger").is(event.target) && !$menu.is(event.target)) {
+      $menu.removeClass("active");
     }
   });
-});
 
-function toggleBackToTopButton() {
-  const backToTopButton = $("#backToTop");
-  if ($(document).scrollTop() > 20) {
-    backToTopButton.css("display", "block");
-  } else {
-    backToTopButton.css("display", "none");
-  }
-}
-
-const menuLinks = $("#menu a");
-
-menuLinks.on("click", function () {
-  removeActiveClassFromLinks();
-  $(this).addClass("active");
-});
-
-$(".hamburger").on("click", function () {
-  $("#menu").toggleClass("active");
-});
-
-$(document).on("click", function (event) {
-  if (!$(".hamburger").is(event.target) && !$("#menu").is(event.target)) {
-    $("#menu").removeClass("active");
-  }
-});
-
-$(".times").on("click", function () {
-  $("#menu").toggleClass("active");
-});
-
-function removeActiveClassFromLinks() {
-  menuLinks.removeClass("active");
-}
-
-
-$(document).ready(function() {
-  $("#logo").hover(
-      function() {
-        $(this).attr("src", "/img/splash_tiny.gif");
-      },
-      function() {
-          $(this).attr("src", "/img/instapatent_tiny.png");
-      }
+  // Hover event listener
+  $logo.hover(
+    function () {
+      $(this).attr("src", "/img/splash_tiny.gif");
+    },
+    function () {
+      $(this).attr("src", "/img/instapatent_tiny.png");
+    }
   );
-});
 
-$(window).scroll(function () {
-  var scrollTop = $(this).scrollTop();
-  $('.background-image').css('background-position', 'center ' + (scrollTop / 1.2) + 'px');
+  // Functions
+  function toggleBackToTopButton() {
+    if ($(document).scrollTop() > 20) {
+      $backToTopButton.css("display", "block");
+    } else {
+      $backToTopButton.css("display", "none");
+    }
+  }
+
+  function toggleMenuScrolledClass() {
+    if ($window.scrollTop() > 0) {
+      $menu.addClass("scrolled");
+    } else {
+      $menu.removeClass("scrolled");
+    }
+  }
+
+  function setActiveLinkOnScroll() {
+    if ($window.scrollTop() === 0) {
+      removeActiveClassFromLinks();
+    }
+
+    $sections.each(function (index, section) {
+      const rect = section.getBoundingClientRect();
+      if (rect.top <= 0 && rect.bottom >= 0) {
+        $menuLinks.removeClass("active");
+        $($menuLinks[index]).addClass("active");
+
+        const id = $(section).attr("id");
+        history.pushState(null, null, "#" + id);
+      }
+    });
+  }
+
+  function removeActiveClassFromLinks() {
+    $menuLinks.removeClass("active");
+  }
+
+  function parallaxBackgroundImage() {
+    var scrollTop = $window.scrollTop();
+    $backgroundImage.css(
+      "background-position",
+      "center " + scrollTop / 1.2 + "px"
+    );
+  }
 });
